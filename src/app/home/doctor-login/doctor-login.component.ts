81d5b080bb23d754daabd5692791eb0868f75a2e
RANJIT KUMAR DAS <103984085+Ranjit7333@users.noreply.github.com>
2023-10-06 18:24:20 +0530

    Update doctor-login.component.ts

--- src/app/home/doctor-login/doctor-login.component.ts
+++ src/app/home/doctor-login/doctor-login.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-doctor-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './doctor-login.component.html',
-  styleUrls: ['./doctor-login.component.css']
+  styleUrls: ['./doctor-login.component.css'],
 })
-export class DoctorLoginComponent {
-
+export class DoctorLoginComponent implements OnInit {
+  ShowFlip:boolean = false;
+  loginSubmitted:boolean = false;
+  ObjDoctor:Doctor = new Doctor();
+  ObjPatentr:Patent = new Patent();
+  show:boolean = false;
+  password :any;
+  masseage:string = '';
+  constructor(private _router: Router ){}
+  ngOnInit(): void {
+    this.password = 'password'
+  }
+  onClick(){
+    if (this.password === 'password') {
+      this.password = 'text';
+      this.show = true;
+    } else {
+      this.password = 'password';
+      this.show = false;
+    }
+  }
+  flipOff(){
+    this.ShowFlip = !this.ShowFlip;
+    this.loginSubmitted = false
+    this.ObjDoctor = new Doctor();
+    this.ObjPatentr = new Patent();
+    this.masseage = ''
+  }
+  Authlogin(valid:any){
+    this.masseage = ''
+    this.loginSubmitted = true
+    if(valid){
+      if(this.ObjDoctor.Password === 'admin' && this.ObjDoctor.Password ==='admin'){
+        console.log('this.ObjDoctor',this.ObjDoctor)
+        this.ObjDoctor = new Doctor();
+        this.loginSubmitted = false
+        this._router.navigate(['/Dashboard'])
+      }
+     else if(this.ObjPatentr.Password === 'admin' && this.ObjPatentr.Password ==='admin'){
+        console.log('this.ObjPatentr',this.ObjPatentr)
+        this.loginSubmitted = false
+        this.ObjPatentr = new Patent();
+        this._router.navigate(['/Dashboard'])
+      }else {
+        this.masseage = 'Invalid Username and Password'
+      }
+    }
+    
+  }
+  
+}
+class Doctor{
+  user_name:any;
+  Password:any;
+}
+class Patent{
+  user_name:any;
+  Password:any;
 }
